c9a15509721f28c5077821115958679bd9bc7055
mohammadammash <mohammadammash31@gmail.com>
2022-09-22 20:49:39 +0300

    Creating the edit-profile popup content in 'profile.html'

--- client-frontend/js/profile.js
+++ client-frontend/js/profile.js
@@ -4,7 +4,9 @@ const reply_popup = document.getElementById('reply-popup');
 const reply_message = document.getElementById('reply-message');
 const reply_message_submit = document.getElementById('reply-submit');
 const main_body = document.getElementById('profile-html');
+const edit_profile_button = document.getElementById('edit-profile-button');
 
+// START OF REPLY POPUP
 // show reply modal whenever any reply button is clicked:
 const showReplyModal = ()=>{
     reply_popup.classList.remove('display-none');
@@ -21,7 +23,16 @@ const submitReply = ()=>{
     console.log(`REPLY MSG: ${reply_message.value}`);
     closeReplyModal();
 }
+// END OF REPLY POPUP
 
+// START OF PROFILE POPUP
+const showEditProfileModal = ()=>{
+    console.log('Profile Modal');
+}
+// END OF PROFILE POPUP
+
+
+// START OF REPLY MODAL EVENT LISTENERS
 // add event listener for each 'REPLY-button' of messages
 for(let btn of reply_buttons){
     btn.addEventListener('click',showReplyModal);
@@ -30,3 +41,9 @@ for(let btn of reply_buttons){
 close_reply_popup.addEventListener('click',closeReplyModal)
 // handling/ testing submit reply button:
 reply_message_submit.addEventListener('click',submitReply);
+// END OF REPLY MODAL EVENT LISTENERS
+
+// START OF EDIT PROFILE MODAL EVENT LISTENERS
+// edit profile button to show modal:
+edit_profile_button.addEventListener('click',showEditProfileModal);
+// START OF EDIT PROFILE MODAL EVENT LISTENERS
