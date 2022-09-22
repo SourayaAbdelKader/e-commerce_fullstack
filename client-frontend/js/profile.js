66faca1482ecd17f6d1752446fbdeffd7cf1dc58
mohammadammash <mohammadammash31@gmail.com>
2022-09-22 20:24:57 +0300

    Adding close reply modal/popup activity, and test submit and input values with basic console.log

--- client-frontend/js/profile.js
+++ client-frontend/js/profile.js
@@ -3,15 +3,30 @@ const close_reply_popup = document.getElementById('close-reply-popup');
 const reply_popup = document.getElementById('reply-popup');
 const reply_message = document.getElementById('reply-message');
 const reply_message_submit = document.getElementById('reply-submit');
-const body = document.getElementById('profile-html');
+const main_body = document.getElementById('profile-html');
 
 // show reply modal whenever any reply button is clicked:
 const showReplyModal = ()=>{
     reply_popup.classList.remove('display-none');
-    body.classList.add('disable-pointer');
+    main_body.classList.add('disable-pointer');
+}
+// close reply modal:
+const closeReplyModal = ()=>{
+    reply_popup.classList.add('display-none');
+    main_body.classList.remove("disable-pointer");
+}
+// submit reply (popup):
+const submitReply = ()=>{
+    console.log('SUBMITTTT');
+    console.log(`REPLY MSG: ${reply_message.value}`);
+    closeReplyModal();
 }
 
 // add event listener for each 'REPLY-button' of messages
 for(let btn of reply_buttons){
     btn.addEventListener('click',showReplyModal);
 }
+// add event listener to close modal:
+close_reply_popup.addEventListener('click',closeReplyModal)
+// handling/ testing submit reply button:
+reply_message_submit.addEventListener('click',submitReply);
