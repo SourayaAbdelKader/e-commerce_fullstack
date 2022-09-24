71a563659c6db8326311d935628a0e4b5f6773c6
mohammadammash <mohammadammash31@gmail.com>
2022-09-24 21:29:14 +0300

    Adding functionality to 'decrementItemQuantity' function locally to fetch post request '/ecommerce-server/update_cart.php' to update it when decreasing the quantity of product in the cart, in '/client-frontend/js/cart.js'

--- client-frontend/js/cart.js
+++ client-frontend/js/cart.js
@@ -1,8 +1,9 @@
 const checkout_submit = document.getElementById("checkout-submit");
 const apply_voucher = document.getElementById("voucher-submit");
 const voucher_code = document.getElementById("voucher-input");
-const cart_total_price = document.getElementById("cart-total");
+const cart_total_show = document.getElementById("cart-total");
 const cart_container = document.getElementById("cart-items");
+var cart_total = 0;
 
 // START OF EVENT LISTENERS FOR EACH ITEM IN CART
 //  remove the whole item/product from the cart/ when X button is clicked:
@@ -13,6 +14,8 @@ const removeItem = (e) => {
 };
 // decrement quantity of single item and update DB
 const decrementItemQuantity = (e) => {
+  //load client_id from localStorage:
+  const client_id = 1;
   const cart_item = e.target.parentNode.parentNode.parentNode;
   const item_id = e.target.parentNode.parentNode.parentNode.children[0].value;
   const quantity = e.target.nextSibling;
@@ -27,14 +30,29 @@ const decrementItemQuantity = (e) => {
   // update quantity shown to user, product total shown to user, and whole cart total
   quantity.textContent = quantity_value;
   total_price.textContent = total_price_value - parseInt(price_per_item_value);
-  cart_total_price.textContent =
-    parseInt(cart_total_price.textContent) - parseInt(price_per_item_value);
+  cart_total_show.textContent =
+    parseInt(cart_total_show.textContent) - parseInt(price_per_item_value);
   if (quantity_value <= 0) {
     cart_item.remove(); //remove the div totally(product removed when quantity becomes <= 0)
     quantity_value = -1; //set it to -1 (since in API update_cart if (== -1) to remove product totally from cart)
   }
 
   // update cart in db both cases ( 0 or positive product quantity)
+  const update_cart = async () => {
+    let params = new URLSearchParams();
+    params.append("client_id", client_id);
+    params.append("product_id", item_id);
+    params.append("new_quantity", quantity_value);
+    const url =
+      "http://localhost/e-commerce_fullstack/ecommerce-server/update_cart.php";
+    await axios
+      .post(url, params)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+  update_cart();
 };
 // increment quantity of single item
 const incrementItemQuantity = (e) => {
@@ -66,6 +84,7 @@ const addItemEventListeners = () => {
 // add item to html content:
 const showItem = (item) => {
   const total_price = parseInt(item.price) * parseInt(item.quantity);
+  cart_total += total_price; //calculate the whole cart total while adding each item
   const itemHTML = `<div class="cart-item">
       <input type="hidden" value="${item.id}" /><div class="item-remove">&times;</div><div class="item-info">
         <img class="item-image" src="${item.main_image}" alt="product-main-image" /><h3 class="item-name">${item.title}</h3>
@@ -74,7 +93,7 @@ const showItem = (item) => {
   cart_container.innerHTML += itemHTML;
 };
 // when loading the page get all the items in user cart:
-const getCartItems = () => {
+const getCartItems = async () => {
   // get client_id from localStorage first
   const client_id = 1; //for testing
   const get_items = async () => {
@@ -94,7 +113,9 @@ const getCartItems = () => {
       .catch((err) => console.log(err));
   };
 
-  get_items();
+  await get_items();
+  // after adding all items show the total of the cart:
+  cart_total_show.textContent = cart_total;
 };
 
 // apply the voucher code added to the total and recalculate the new total
@@ -103,7 +124,7 @@ const applyVoucher = () => {
 };
 // submit the cart to checkout -> turn isCheckout bool to true and create new empty car for user:
 const checkoutOrder = () => {
-  console.log(cart_total_price.textContent);
+  console.log(cart_total_show.textContent);
 };
 // END OF EVENT LISTENER FUNCTIONS
 
