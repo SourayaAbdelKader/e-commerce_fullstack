6fb748810a4bf97d2125c52922d99d52726043bd
tahataha005 <taha.f.taha0@gmail.com>
2022-09-24 18:30:11 +0300

    Creating favorite_products function that adds click event listners to all favorite icons

--- client-frontend/js/products.js
+++ client-frontend/js/products.js
@@ -1,8 +1,36 @@
 const client_id = 1;
 
-const productsWrapper = document.getElementById("productsWrapper")
-const getProductApi = "http://localhost/e-commerce_fullstack/backend/get_products.php"
-const addFavouriteApi = "http://localhost/e-commerce_fullstack/backend/add_favorite.php"
+const productsWrapper = document.getElementById("productsWrapper");
+const getProductApi = "http://localhost/e-commerce_fullstack/backend/get_products.php";
+const addFavouriteApi = "http://localhost/e-commerce_fullstack/backend/add_favorite.php";
+
+
+
+
+// function that adds to all like icons an event listner for fetching in a later function
+const favorite_products = () => {
+  const likes = document.querySelectorAll(".like");
+  likes.forEach(like =>{
+    let product_id = like.getAttribute("data-value");
+    like.addEventListener("click", () =>{
+      add_favourite(product_id);
+    })
+  })
+}
+
+// function adding a favorite item to the database
+const add_favourite = async (product_id) => {
+  let params = new URLSearchParams();
+  params.append("client_id", client_id);
+  params.append("product_id", product_id);
+  await axios
+    .post(addFavouriteApi,params)
+    .then((data) => {
+      // ouput whether product is in favorite or not
+      console.log(data.data);
+    })
+    .catch((err) => console.log(err));
+};
 
 
 // function fetching all products from database
