db7aaa3fa45395b9d7954b24afd3839bc36ab259
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-25 08:15:37 +0300

    adding APIs to receive the disocunts and ads

---
+++ seller/seller-backend/receive_ad.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+include("connection.php");
+
+$id = $_POST["product_id"];
+
+$query = $mysqli -> prepare("SELECT * FROM advertisements WHERE product_id = ?");
+$query->bind_param('s', $id);
+$query -> execute();
+$array = $query -> get_result();
+
+$response = [];
+
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
+
+$json = json_encode($response);
+echo $json;
+?>
