a935b75c13d4d14841094c1ba6d77200ae6efb94
mohammadammash <mohammadammash31@gmail.com>
2022-09-25 17:58:21 +0300

    Selecting nav-links and adding functionality to each button, in './js/show.js'

--- client-frontend/js/show.js
+++ client-frontend/js/show.js
@@ -4,6 +4,10 @@ const seller_info = document.getElementById("seller-info");
 const item_description_button = document.getElementById("description-button");
 const item_description = document.getElementById("item-description");
 item_description_button.style.backgroundColor = "#94a9b4"; //by default
+// navlinks:
+const signout_button = document.getElementById("signout-button");
+const go_to_cart_button = document.getElementById("go-to-cart-button");
+const back_to_products_button = document.getElementById("back-to-products");
 
 const showItemDescription = () => {
   seller_info.classList.add("display-none");
@@ -20,3 +24,17 @@ const showSellerInfo = () => {
 
 seller_info_button.addEventListener("click", showSellerInfo);
 item_description_button.addEventListener("click", showItemDescription);
+
+//Start of Event listeners linking to another pages:
+//
+signout_button.addEventListener("click", () => {
+  localStorage.clear();
+  window.location.href = "./register.html";
+});
+go_to_cart_button.addEventListener("click", () => {
+  window.location.href = "./cart.html";
+});
+back_to_products_button.addEventListener("click", () => {
+  window.location.href = "./products.html";
+});
+//End of Event listeners linking to another pages:
