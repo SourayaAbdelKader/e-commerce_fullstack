bcfca0aab6cc355d9b7b5040a2fefaf1cc19072f
mohammadammash <mohammadammash31@gmail.com>
2022-09-25 19:06:07 +0300

    Selecting buttons of item-information/right-section with testing console.log within, in '/js/show.js'

--- client-frontend/js/show.js
+++ client-frontend/js/show.js
@@ -8,6 +8,10 @@ item_description_button.style.backgroundColor = "#94a9b4"; //by default
 const signout_button = document.getElementById("signout-button");
 const go_to_cart_button = document.getElementById("go-to-cart-button");
 const back_to_products_button = document.getElementById("back-to-products");
+// buttons:
+const add_to_cart_button = document.getElementById("cart-button");
+const add_to_favorite_button = document.getElementById("fav-button");
+const add_to_wishlist_button = document.getElementById("wish-button");
 
 const showItemDescription = () => {
   seller_info.classList.add("display-none");
@@ -38,3 +42,33 @@ back_to_products_button.addEventListener("click", () => {
   window.location.href = "./products.html";
 });
 //End of Event listeners linking to another pages:
+
+// START OF POST APIs (add_to_cart && add_favorite && add_wishlist)
+const addToFavorites = ()=>{
+  console.log('fav');
+  const user = JSON.parse(localStorage.getItem('user'));
+  const client_id = user.id;
+  const product_id = JSON.parse(localStorage.getItem('product_id'));
+  console.log(client_id, product_id);
+}
+const addToWishlist = () => {
+  console.log("wishlist");
+  const user = JSON.parse(localStorage.getItem("user"));
+  const client_id = user.id;
+  const product_id = JSON.parse(localStorage.getItem("product_id"));
+  console.log(client_id, product_id);
+};
+const addToCart = () => {
+  console.log("cart");
+  const user = JSON.parse(localStorage.getItem("user"));
+  const client_id = user.id;
+  const product_id = JSON.parse(localStorage.getItem("product_id"));
+  const quantity = 1; 
+  console.log(client_id, product_id, quantity);
+};
+// END OF POST APIs
+
+// Event listeners for APIs (add_to_cart && add_favorite && add_wishlist):
+add_to_favorite_button.addEventListener('click',addToFavorites);
+add_to_wishlist_button.addEventListener("click", addToWishlist);
+add_to_cart_button.addEventListener('click',addToCart);
