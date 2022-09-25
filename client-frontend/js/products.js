477dd5f1289c92102db471c13892d2d9b20bbed6
mohammadammash <mohammadammash31@gmail.com>
2022-09-25 20:52:06 +0300

    updating the wishlist wrapper id to empty the wishlist each time we load intially, so we avoid items repitition successfully in the wishlist section, in '/js/products.js'

--- client-frontend/js/products.js
+++ client-frontend/js/products.js
@@ -38,6 +38,7 @@ function random_int(min, max) {
 // Function getting products as object and appending them to a wrapper
 const load_products = (products, wrapper) => {
   console.log(products, wrapper);
+  wrapper.innerHTML = '';
   if (products.length != 0) {
     products.forEach((product) => {
       // add seller name
